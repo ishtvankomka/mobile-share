02f032573c130d78134cf9b61421f982c7ff8d3d
“ishtvankomka” <ishtvan.komonii.it.2021@lpnu.ua>
2023-07-04 00:05:40 +0300

    Feat: addeventlistener test adapted pt.9

--- src/App.js
+++ src/App.js
@@ -138,6 +138,28 @@ function App() {
   }
 
 
+  function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
+}
+
+
+
   const ref = useRef(null);
 
   useEffect(() => {
@@ -147,8 +169,11 @@ function App() {
       type: "text/plain",
     });
 
+    var blob = dataURItoBlob(url);
+
+
     const handleClick = event => {
-      share2('title', 'text', url)
+      share2('title', 'text', blob)
     };
 
     element.addEventListener('click', handleClick);
