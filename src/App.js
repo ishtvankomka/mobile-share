7f1e33be78c3f0913f515060230d8986d3550480
“ishtvankomka” <ishtvan.komonii.it.2021@lpnu.ua>
2023-07-04 22:52:18 +0300

    Feat: replaced not working share functions by one real

--- src/App.js
+++ src/App.js
@@ -5,206 +5,6 @@ import { toPng } from 'html-to-image';
 function App() {
   const shareRef = useRef(null)
 
-  const handleOnDownload = () => {
-    toPng(shareRef.current, { cacheBust: false })
-      .then((dataUrl) => {
-        console.log('download dataUrl: ', dataUrl)
-        const link = document.createElement("a")
-        link.download = ''
-        link.href = dataUrl
-        link.click()
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }
-
-  function dataURLtoFile(dataurl, filename) {
-    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
-      bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-    return new File([u8arr], filename, { type: mime });
-  }
-
-  const handleOnShare1 = () => {
-    toPng(shareRef.current, { cacheBust: false })
-      .then((dataUrl) => {
-        console.log('share dataUrl: ', dataUrl)
-        const files = [dataURLtoFile(dataUrl, 'test')]
-        console.log('share files: ', files)
-        try {
-          navigator.share({
-            files,
-            title: "Images",
-            text: "Beautiful images",
-          });
-        } catch (err) {
-          alert(err)
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        alert(err)
-      })
-  }
-
-  function urltoFile(url, filename, mimeType) {
-    mimeType = mimeType || (url.match(/^data:([^;]+);/) || '')[1];
-    return (fetch(url)
-      .then(function (res) { return res.arrayBuffer(); })
-      .then(function (buf) { return new File([buf], filename, { type: mimeType }); })
-    );
-  }
-
-  const [url, setUrl] = useState('')
-  const [urlFile, setUrlFile] = useState(null)
-  useEffect(() => {
-    if (urlFile)
-      console.log('urlFile: ', urlFile)
-  }, [urlFile])
-  const [file, setFile] = useState(null)
-  useEffect(() => {
-    if (file)
-      console.log('file: ', file)
-  }, [file])
-  useEffect(() => {
-    if (shareRef)
-      toPng(shareRef.current, { cacheBust: false })
-        .then((dataUrl) => {
-          fetch(dataUrl)
-            .then(response => response.blob())
-            .then(blob => {
-              const file = new File([blob], "sample.png", { type: blob.type })
-              console.log(file)
-              setUrlFile(file)  //File object
-            })
-          setUrl(dataUrl)
-          console.log('share dataUrl: ', dataUrl)
-          setFile(dataURLtoFile(dataUrl, 'test'))
-        })
-  }, [shareRef])
-
-  const handleOnShare2 = () => {
-    toPng(shareRef.current, { cacheBust: false })
-      .then((dataUrl) => {
-        console.log('share dataUrl: ', dataUrl)
-        const files = [urltoFile(dataUrl, 'test', 'image/png')]
-        console.log('share files: ', files)
-        try {
-          navigator.share({
-            files,
-            title: "Images",
-            text: "Beautiful images",
-          });
-        } catch (err) {
-          alert(err)
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }
-
-  const blob = fetch('https://cdn.glitch.com/f96f78ec-d35d-447b-acf4-86f2b3658491%2Fchuck.png?v=1618311092497').then(r => r.blob())
-  const share = async (title, text, blob) => {
-    const data = {
-      files: [
-        new File([blob], 'file.png', {
-          type: blob.type,
-        }),
-      ],
-      title: title,
-      text: text,
-    };
-    try {
-      await navigator.share(data);
-    } catch (err) {
-      console.error(err.name, err.message);
-      alert(err)
-    }
-  };
-
-  const handleOnShare3 = () => {
-    share('title', 'text', blob)
-  }
-
-  const share2 = async (title, text, file) => {
-    const data = {
-      files: [file],
-      title: title,
-      text: text,
-    };
-    try {
-      await navigator.share(data);
-    } catch (err) {
-      console.error(err.name, err.message);
-      alert(err)
-    }
-  };
-
-  const handleOnShare4 = () => {
-    share2('title', 'text', file)
-  }
-
-
-  function dataURItoBlob(dataURI) {
-    if (dataURI.length) {
-      // convert base64/URLEncoded data component to raw binary data held in a string
-      var byteString;
-      if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-      else
-        byteString = unescape(dataURI.split(',')[1]);
-
-      // separate out the mime component
-      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-      // write the bytes of the string to a typed array
-      var ia = new Uint8Array(byteString.length);
-      for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-      }
-
-      return new Blob([ia], { type: mimeString });
-    }
-  }
-
-
-
-  const ref = useRef(null);
-
-  useEffect(() => {
-    if (url) {
-      const element = ref.current;
-
-      /* const text = new File(["foo"], "foo.txt", {
-        type: "text/plain",
-      }); */
-
-      const blob = dataURItoBlob(url);
-      const result_file = new File([blob], "file.png", {
-        type: "image/png",
-      });
-
-      console.log('result_file: ', result_file)
-
-      const handleClick = event => {
-        share2('title', 'text', result_file)
-      };
-
-
-      element.addEventListener('click', handleClick);
-
-      return () => {
-        element.removeEventListener('click', handleClick);
-      };
-    }
-  }, [url]);
-
-
-
   const [pngUrl, setPngUrl] = useState(null)
   useEffect(() => {
     if (shareRef)
@@ -213,6 +13,7 @@ function App() {
           setPngUrl(dataUrl)
         })
   }, [shareRef])
+
   const [pngBlob, setPngBlob] = useState(null)
   useEffect(() => {
     if (pngUrl?.length) {
@@ -229,6 +30,7 @@ function App() {
       setPngBlob(new Blob([ia], { type: mimeString }))
     }
   }, [pngUrl])
+
   const [pngFile, setPngFile] = useState(null)
   useEffect(() => {
     if (pngBlob)
@@ -239,7 +41,7 @@ function App() {
       )
   }, [pngBlob])
 
-  const handleOnShare7 = async (event) => {
+  const handleOnShare = async (event) => {
     if (pngFile) {
       const data = {
         files: [pngFile],
@@ -260,44 +62,17 @@ function App() {
     <div className="App">
       <div className='block'>
         <div className='flag' ref={shareRef}>
-          <div className='blue flag-color'>
-
-          </div>
-          <div className='yellow flag-color'>
-
-          </div>
+          <div className='blue flag-color'></div>
+          <div className='yellow flag-color'></div>
         </div>
         <div className='control'>
-          <div onClick={() => { handleOnDownload() }}>
-            <p>Download</p>
-          </div>
-          <div onClick={() => { handleOnShare1() }}>
-            <p>Share 1</p>
-          </div>
-          <div onClick={() => { handleOnShare2() }}>
-            <p>Share 2</p>
-          </div>
-          <div onClick={() => { handleOnShare3() }}>
-            <p>Share 3</p>
-          </div>
-          <div onClick={() => { handleOnShare4() }}>
-            <p>Share 4</p>
-          </div>
-          <button onClick={() => { handleOnShare4() }}>
-            <p>Share 5</p>
-          </button>
-          <button ref={ref} type="button">
-            <p>Share 6</p>
-          </button>
-          <button onClick={(e) => { handleOnShare7(e) }}>
-            <p>Share 7</p>
+          <button onClick={(e) => { handleOnShare(e) }}>
+            <p>Share</p>
           </button>
         </div>
       </div>
     </div>
   );
-
-
 }
 
 export default App;
